58e81b066dc5450bfa0b5e156c22ddc3f673b86c
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2020-12-08 18:58:27 +0600

    created apiviews.py

---
+++ polls/apiviews.py
@@ -0,0 +1,20 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import Poll, Choice
+from .serializers import PollSerializer
+
+
+class PollList(APIView):
+    def get(self, request):
+        MAX_OBJECTS = 20
+        polls = Poll.objects.all()[:MAX_OBJECTS]
+        data = PollSerializer(polls, many=True).data
+        return Response(data)
+
+
+class PollDetail(APIView):
+    def get(self, request, pk):
+        poll = get_object_or_404(Poll, pk=pk)
+        data = PollSerializer(poll).data
+        return Response(data)
--- polls/serializers.py
+++ polls/serializers.py
@@ -24,3 +24,12 @@ class PollSerializer(serializers.ModelSerializer):
     class Meta:
         model = Poll
         fields = '__all__'
+
+
+"""
+What have we got with this? The PollSerializer class has a number of methods,
+•	A is_valid(self, ..) method which can tell if the data is sufficient and valid to create/update a model instance.
+•	A save(self, ..) method, which knows how to create or update an instance.
+•	A create(self, validated_data, ..) method which knows how to create an instance. This method can be overriden to customize the create behaviour.
+•	A update(self, instance, validated_data, ..) method which knows how to update an instance. This method can be overriden to customize the update behaviour.
+"""
