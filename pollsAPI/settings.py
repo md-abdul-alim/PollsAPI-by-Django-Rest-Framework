a1696cc12c4bfed32539c78f08f26ba94e8d9952
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2020-12-08 15:40:31 +0600

    Poll model created

--- pollsAPI/settings.py
+++ pollsAPI/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # new
+    'polls',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
