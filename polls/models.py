e74846314045bd84066ffd7b4f6b6aa9a2d713b9
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2020-12-08 15:43:12 +0600

    Updated Models.py
    Choice model created

--- polls/models.py
+++ polls/models.py
@@ -10,3 +10,12 @@ class Poll(models.Model):
 
     def __str__(self):
         return self.question
+
+
+class Choice(models.Model):
+    pool = models.ForeignKey(
+        Poll, related_name='choices', on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.choice_text
