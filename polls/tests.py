633cb506b4dc06e3ddd5ee0b48ae513e8fd8a1a4
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2020-12-10 01:32:38 +0600

    Updated tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -1,10 +1,69 @@
+from django.http import request
 from django.test import TestCase
 from rest_framework.test import APITestCase
 from rest_framework.test import APIRequestFactory
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 from polls import apiviews
+from rest_framework.test import APIClient
 # Create your tests here.
 
 
+# second run
+#this is not working
+class TestPoll(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+        self.factory = APIRequestFactory()
+        self.view = apiviews.PollViewSet.as_view({'get': 'list'})
+        self.url = '/polls'
+        self.user = self.setup_user()
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+
+    @staticmethod
+    def setup_user():
+        User = get_user_model()
+        return User.objects.create_user(
+            'test', email='testuser@test.com', password='test'
+        )
+
+    def test_list(self):
+        request = self.factory.get(self.url,
+                                   HTTP_AUTHORIZATION='Token {}'.format(self.token.key))
+        request.user = self.user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200,
+                         'Expected Response Code 200, received {0} instead.'
+                         .format(response.status_code))
+
+    def test_list2(self):
+        self.client.login(username='milon', password='adminmilon')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200,
+                         'Expected Response Code 200, received {0} instead.'
+                         .format(response.status_code)
+                         )
+
+    def test_create(self):
+        self.client.login(username='milon', password='adminmilon')
+        params = {
+            "question": "How are you?",
+            "created_by": 1
+        }
+        response = self.client.post(self.url, params)
+        self.assertEqual(response.status_code, 201,
+                         'Expected Response Code 201, received {0} instead.'
+                         .format(response.status_code)
+                         )
+
+
+"""
+#first run
+#this is working
+#this is for test purpose.if user not authentication apiviews can not access
 class TestPoll(APITestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
@@ -18,3 +77,4 @@ class TestPoll(APITestCase):
                          'Expected Response Code 200, received {0} instead.'
                          .format(response.status_code)
                          )
+"""
